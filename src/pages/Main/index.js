aca001a49dbfda28a55071e3a3948b02552ea57b
marcelooo616 <marcelooo2014@hotmail.com>
2023-07-29 01:42:56 -0300

    feat - Estilizando pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
+
+import {FaGithub, FaPlus} from 'react-icons/fa'
 
 export default function Home() {
     return(
-        <Title>
-            Main    
-        </Title>
+        <Container>
+            
+            <h1>
+                <FaGithub size={25}/>
+                Meus Repositorios
+            </h1>
+
+
+            <Form onSubmit={()=>{}}>
+                <input type={'text'} placeholder='Adicionar Repositorios'/>
+                <SubmitButton>
+                    <FaPlus color="#fff" size={14}/>
+            
+                </SubmitButton>    
+            </Form>    
+        </Container>
     )
 }
